56b817b821180de7a8ef82623f2b65e850c55d96
weschen <chenxuhua0530@gmail.com>
2022-05-12 10:57:32 +0800

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+# card_scaner
+
+CH34 Card Scaner，刷卡器Flutter插件，内部项目使用
+
+芯片使用的是[沁恒的方案](http://www.wch.cn/products/CH340.html)
---
+++ example/android/app/src/main/java/com/cf/card_scaner_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cf.card_scaner_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
